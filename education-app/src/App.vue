5692c1c136f1229cb83e2be5201e0d01a19053e5
igor <igori.kalugin@mail.ru>
2023-06-10 18:52:44 +0300

    added application header

--- education-app/src/App.vue
+++ education-app/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
+    <header>
+      <div v-if="userData" class="user-info">
+        Привет, {{ userData.login }}!
+      </div>
+      <router-link v-if="isLoginPage" to="/register">Регистрация</router-link>
+      <router-link v-else-if="isRegisterPage" to="/login">Войти</router-link>
+      <router-link v-else to="/login">Выйти</router-link>
+    </header>
     <router-view @userLoggedIn="handleUserLoggedIn"> </router-view>
+    
   </div>
 </template>
 
@@ -11,6 +20,14 @@ export default {
     return {
       userData: null, // Переменная для хранения данных пользователя
     };
+  },
+  computed: {
+    isLoginPage() {
+      return this.$route.name === 'Login';
+    },
+    isRegisterPage() {
+      return this.$route.name === 'Register';
+    },
   },
   methods: {
     handleUserLoggedIn(userData) {
@@ -27,6 +44,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 20px;
+  margin: 0;
+}
+
+header {
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px;
+  border-bottom: #6ec9f7fb;
+  border-bottom-style: groove;
+  border-width: 2px;
+}
+
+header a {
+  margin-left: 10px;
+  text-decoration: none;
+  color: #0b7beb;
+}
+
+.user-info {
+  font-weight: bold;
+  margin-right: 2%;
 }
 </style>
